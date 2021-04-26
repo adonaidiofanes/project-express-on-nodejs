0f558d15910da14ae96fe52fc0c6e670bec52472
Adonai Diofanes <adonaibm@gmail.com>
2021-04-26 18:49:01 -0300

    Create migration and findAll on route.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+// var User = require("../models/user");
+
+const model = require('../models'); // importing all the database models in the new constant
+const User = model.User; // creating a constant of user model from the models
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const users = await User.findAll();
+  console.log(users);
+  res.end(JSON.stringify(users));
+  // console.log("Jane's auto-generated ID:", jane.id);
+  // res.send('dog');
+  // const users = await User.create({ firstName: "Jane", lastName: "Doe" });
 });
 
 module.exports = router;
