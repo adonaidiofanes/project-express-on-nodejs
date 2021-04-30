79b29f793e30947da61c9e38d686c0ac9b4710eb
Adonai Diofanes <adonaibm@gmail.com>
2021-04-30 12:39:24 -0300

    Scripts to tests

---
+++ script.js
@@ -0,0 +1,12 @@
+const model = require('./models');
+const { Task, Project, User, Address } = model;
+
+
+(async () => {
+    // const tasks = await Task.findOne({include: [{all:true}]});
+    // console.log(tasks);
+
+    // Find address of user
+    // const user = await User.findOne({include: [{all:true}]});
+    // console.log(user.Address);
+})();
